8c009f1fa9dfe51c0dbb5889bf10c9331e66df75
sfGLC <sfan@glc.org>
2018-05-25 18:02:36 -0400

    more

--- Tester.py
+++ Tester.py
@@ -1,8 +1,23 @@
 from TwoSum import Solution
 
-nums = [2,7,11,15, 3,3]
-target = 6
-s = Solution()
 
-result = s.twoSum(nums, target)
+solution = Solution()
+
+# nums = [2,7,11,15, 3,3]
+# target = 6
+# result = solution.twoSum(nums, target)
+
+# s = "dvdf"
+# result = solution.lengthOfLongestSubstring(s)
+
+# val = 1534236469
+# result = solution.reverse(val)
+
+# val = 10
+# result = solution.isPalindrome(val)
+
+s = "PAYPALISHIRING"
+numRows = 4
+result = solution.convert(s, numRows)
+
 print result
--- TwoSum.py
+++ TwoSum.py
@@ -47,3 +47,116 @@ class Solution(object):
         #     val2 = target - val1
         #     if val2 in d.values():
         #         return [d[val1], d[val2]]
+
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        strSet = [0, 1]
+        index = 0
+        i = 0
+        while i < len(s):
+            char = s[i]
+            substr = s[strSet[0]:i]
+            if s[i] in s[strSet[0]:i]:
+
+                i = strSet[0] + s[strSet[0]:i].index(s[i])
+                v1 = s.index(s[strSet[0]:i])
+                v2 = substr.index(s[i])
+                # strSet = list()
+                i+= 1
+                strSet[0] = i
+                continue
+            else:
+                i += 1
+                if result < i - strSet[0]:
+                    result = i - strSet[0]
+
+
+        # for i in range(0, len(s)):
+        #     char = s[i]
+        #     substr = s[strSet[0]:i]
+        #     if s[i] in s[strSet[0]:i]:
+        #         if result < i - strSet[0]:
+        #             result = i - strSet[0]
+        #         i = s.index(s[strSet[0]:i]) + s[strSet[0]:i].index(s[i])
+        #         v1 = s.index(s[strSet[0]:i])
+        #         v2 = substr.index(s[i])
+        #         # strSet = list()
+        #         strSet[0] = i
+                # strSet[1] = i+1
+                # i += 1
+            # else:
+            #     strSet[1] = i
+        return result
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < -2 ** 31 or x > 2 ** 31 - 1:
+            return 0
+        y = x
+        if x < 0:
+            y = -x
+        l = list()
+        while y:
+            l.append(y % 10)
+            y = y / 10
+        l.reverse()
+        result = 0
+        while l:
+            result = result * 10 + l.pop()
+        if x < 0:
+            result = -result
+        return result
+
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            l = list()
+            while x:
+                l.append(x % 10)
+                x = x / 10
+            for i in range(len(l) / 2):
+                if l[i] != l[-(i+1)]:
+                    return False
+        return True
+
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        print s
+        if len(s) == 0:
+            return ""
+        l = list()
+        cycle = numRows + numRows - 2
+        for i in range(numRows):
+            l.append([])
+        j = 0
+        print l
+        print cycle
+        while j < len(s):
+            for n in range(cycle):
+                if n >= numRows:
+                    n = numRows - n - 2
+                if j < len(s):
+                    l[n].append(s[j])
+                j += 1
+        print l
+        result = ""
+        for i in range(numRows):
+            while len(l[i]):
+                result += l[i].pop(0)
+        return result
